b84568958c00cb2b928169af1bc79f4dfde179e9
cruzken <frostbytee@gmail.com>
2018-05-07 14:07:04 -0400

    removed auth files, added link model/controller/migration

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,3 +21,11 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Link::class, function (Faker $faker) {
+    return [
+        'url' => $faker->url,
+        'hash' => str_random(4),
+    ];
+});
+
